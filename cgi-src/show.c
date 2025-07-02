cbf0c147043fe5d8d75864d5b2c273b1f1423b0b
Samuel Costa <161505787+coxioxi@users.noreply.github.com>
2025-07-02 14:01:53 -0400

    Add files via upload

---
+++ cgi-src/show.c
@@ -0,0 +1,160 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int
+main ()
+{
+  // Starter HTML (to avoid copy-and-paste annoynances):
+  printf ("<html lang=\"en\">\n");
+  printf ("  <head>\n");
+  printf ("    <title>File Hash Database</title>\n");
+  printf ("    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn."
+          "com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\""
+          "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/"
+          "iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\" />\n");
+  printf ("  </head>\n\n");
+
+  // These variables can be read from the environment using getenv().
+  char *db = getenv ("db");
+  char *record = getenv ("record");
+  char *hash = getenv ("hash");
+  char *query = getenv ("QUERY_STRING");
+  // This is an HTML comment. It's useful for debugging to see if your
+  // environment variables got through.
+  printf ("  <!-- Environment variables:\n");
+  printf ("       db: %s\n", db);
+  printf ("       record: %s\n", record);
+  printf ("       hash: %s\n", hash);
+  printf ("       query: %s\n", query);
+  printf ("    -->\n\n");
+
+  // TODO [PART]: Read the data/data.txt file and produce an HTML table
+  // to match the output in the cgi-src/tests/expected/PART_show_all.txt
+  // file. Note that each line in the data.txt file should correspond to
+  // two "col" divs, and all but the last line should be followed by a
+  // "w-100" div. The overall framework of the body should look like this
+  // (with exactly 2 spaces before the opening < character of each line):
+  //    <body>
+  //      <div class="container">
+  //        <br />
+  //        <h2 class="mb-0">Database Records</h2>
+  //        <div class="row">
+  //          <div class="col py-md-2 border bg-light">PART_hello.txt</div>
+  //          <div class="col py-md-2 border bg-light">94079f...</div>
+  //          <div class="w-100"></div>
+  //          <div class="col py-md-2 border bg-light">PART_show_all.txt</div>
+  //          <div class="col py-md-2 border bg-light">9e5543...</div>
+  //        </div>
+  //      </div>
+  //    </body>
+
+  printf ("  <body>\n");
+  printf ("    <div class=\"container\">\n");
+  printf ("      <br />\n");
+  printf ("      <h2 class=\"mb-0\">Database Records</h2>\n");
+  printf ("      <div class=\"row\">\n");
+
+  if (query != NULL) // Query is set
+    {
+      // If QUERY_STRING is set, use that and split it apart at the & character
+      char *token;
+      const char *delim = "=&";
+
+      token = strtok (query, delim);
+
+      while (token != NULL)
+        {
+          if (strcmp (token, "db") == 0)
+            {
+              db = strtok (NULL, delim);
+            }
+          else if (strcmp (token, "record") == 0)
+            {
+              record = strtok (NULL, delim);
+            }
+          else if (strcmp (token, "hash") == 0)
+            {
+              hash = strtok (NULL, delim);
+            }
+
+          token = strtok (NULL, delim);
+        }
+    }
+
+  if (db == NULL)
+    {
+      db = "data.txt";
+    }
+
+  char realdb[256] = "data/";
+  strcat (realdb, db);
+
+  FILE *file = fopen (realdb, "r");
+  char filename[1024];
+  char nums[1024];
+  int i = 0;
+
+  int recnum = -1;
+
+  if (record != NULL)
+    {
+      recnum = atoi (record);
+    }
+
+  while (fscanf (file, "%s", nums) == 1)
+    {
+      fscanf (file, "%s", filename);
+
+      if (i != 0 && recnum == -1)
+        printf ("        <div class=\"w-100\"></div>\n");
+
+      if (i + 1 == recnum || recnum == -1)
+        {
+          printf (
+              "        <div class=\"col py-md-2 border bg-light\">%s</div>\n",
+              filename);
+
+          if (recnum != -1 && recnum == i + 1 && hash != NULL
+              && strcmp (hash, nums) != 0)
+            printf (
+                "        <div class=\"col py-md-2 border bg-light\">%s <span "
+                "class=\"badge badge-danger\">MISMATCH</span></div>\n",
+                nums);
+          else
+            printf ("        <div class=\"col py-md-2 border "
+                    "bg-light\">%s</div>\n",
+                    nums);
+        }
+
+      ++i;
+    }
+
+  printf ("      </div>\n");
+  printf ("    </div>\n");
+  printf ("  </body>\n");
+
+  fclose (file);
+
+  // TODO [MIN]: Once you have the basic structure working, extend it to
+  // read in environment variables (db, record, hash, and QUERY_STRING).
+  // From a logic standpoint, if QUERY_STRING is set, use that and split it
+  // apart at the & character. For example, the QUERY_STRING might look like:
+  //   db=foo.txt&record=2&hash=9e5543354d4592db8272b3c3e14953770df88ba3
+  // If the QUERY_STRING is not set, look for the db, record, and hash
+  // environment variables independently. If the hash variable is set,
+  // compare its value with the hash value for the specified record. (If
+  // record is not set, then ignore the hash variable.) If the hash does not
+  // match, add this code just after the hash value from the database (put
+  // a space before the <span and no space between </span></div>):
+  //    <span class="badge badge-danger">MISMATCH</span>
+
+  printf ("\n</html>\n");
+
+  return 0;
+}
---
+++ cgi_response.c
@@ -0,0 +1,236 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define BUFFER_LENGTH 8192
+
+#define CRLF "\r\n"
+
+/* Used to execute a given CGI program in a separate process. Uses the
+   version parameter to build the HTTP header. Other parameters are used
+   for the FULL implementation only as follows:
+
+     if method is "GET":
+       Use query as the value of the QUERY_STRING environment variable.
+
+     if method is "POST":
+       Use boundary to split the body of the request (see the project
+       page for an example). The size parameter is only used if the POST
+       request is for uploading a file, and is the size of the data to
+       be uploaded.
+ */
+char *
+cgi_response (char *uri, char *version, char *method, char *query,
+              ssize_t size, char *boundary, char *body)
+{
+
+  // TODO [PART]: If the URI exists and is executable, run it as a separate
+  // process, redirecting its STDOUT back to this process. You can then use
+  // that resulting string to determine the Content-Length to send back. As
+  // an example, here would be the full response to return for
+  // cgi-bin/hello.cgi (assuming HTTP/1.0 and CRLF means "\r\n"). Don't print
+  // the quotes:
+  //   "HTTP/1.0 200 OK" CRLF
+  //   "Content-Type: text/html; charset: UTF-8" CRLF
+  //   "Content-Length: 95" CRLF
+  //   CRLF
+  //   "<html>\n"
+  //   "<head>\n"
+  //   "  <title>Hello world demo</title>\n"
+  //   "</head>\n"
+  //   "\n"
+  //   "<h2>Hello world!</h2>\n"
+  //   "</body>\n"
+  //   "</html>\n"
+
+  char *response = NULL;
+  if (uri != NULL)
+    {
+
+      int pipes[2];
+      pipe (pipes);
+      pid_t child = fork ();
+
+      if (child == 0)
+        {
+          close (pipes[0]);
+          dup2 (pipes[1], STDOUT_FILENO);
+
+          /*
+          if method is "GET":
+          Use query as the value of the
+          QUERY_STRING environment variable.
+          */
+          if (!strcmp (method, "GET"))
+            {
+              if (query)
+                {
+
+                  char queryString[1024];
+                  strcpy (queryString, "QUERY_STRING=");
+                  strcat (queryString, query);
+                  char *env[] = { queryString, NULL };
+                  execle (uri, uri, NULL, env);
+                }
+              else
+                {
+                  execl (uri, uri, NULL);
+                }
+            }
+
+          /*
+          if method is "POST":
+          Use boundary to split the body
+          of the request (see the project page for an example). The size
+          parameter is only used if the POST request is for uploading a file,
+          and is the size of the data to be uploaded.
+          */
+          else if (!strcmp (method, "POST"))
+            {
+              char *db = NULL;
+              char *hash = NULL;
+              char *record = NULL;
+              char *string = NULL;
+
+              string = strstr (body, boundary);
+              string += strlen (boundary);
+              string = strstr (string, "name=\"");
+              int start;
+              int end;
+              char token[1024];
+              char type[1024];
+              char current[1024];
+
+              int index = 0;
+              char *env[] = { NULL, NULL, NULL, NULL };
+
+              while (string)
+                {
+                  strcpy (token, string + 6);
+                  start = (intptr_t)string + 6;
+                  string = strstr (string, "\"\r\n");
+                  end = (intptr_t)string;
+                  strncpy (type, token, (size_t)end - start);
+                  type[end - start] = '\0';
+
+                  string = strstr (string, "\r\n\r\n");
+                  strcpy (token, string + 4);
+
+                  string += 4;
+                  start = (intptr_t)string;
+
+                  string = strstr (string, "\r\n");
+                  end = (intptr_t)string;
+
+                  strncpy (current, token, (size_t)end - start);
+
+                  current[end - start] = '\0';
+
+                  if (!strcmp (type, "db"))
+                    {
+                      db = malloc (strlen ("db=") + strlen (current) + 1);
+                      snprintf (db, strlen ("db=") + strlen (current) + 1,
+                                "db=%s", current);
+                      env[index++] = db;
+                    }
+                  else if (!strcmp (type, "hash"))
+                    {
+                      hash = malloc (strlen ("hash=") + strlen (current) + 1);
+                      snprintf (hash, strlen ("hash=") + strlen (current) + 1,
+                                "hash=%s", current);
+                      env[index++] = hash;
+                    }
+                  else if (!strcmp (type, "record"))
+                    {
+                      record
+                          = malloc (strlen ("record=") + strlen (current) + 1);
+                      snprintf (record,
+                                strlen ("record=") + strlen (current) + 1,
+                                "record=%s", current);
+                      env[index++] = record;
+                    }
+
+                  string = strstr (string, "name=\"");
+                }
+              execle (uri, uri, NULL, env);
+            }
+          // execl(uri, uri, NULL);
+          exit (1);
+        }
+
+      close (pipes[1]);
+
+      char buffer[BUFFER_LENGTH];
+      memset (&buffer, 0, BUFFER_LENGTH);
+
+      ssize_t bytes = read (pipes[0], buffer, BUFFER_LENGTH);
+
+      if (bytes < 0)
+        {
+          return NULL;
+        }
+      ssize_t length;
+
+      if (!strcmp (version, "HTTP/1.0"))
+        {
+          length = snprintf (NULL, 0,
+                             "%s 200 OK\r\n"
+                             "Content-Type: text/html; charset=UTF-8\r\n"
+                             "Content-Length: %ld\r\n\r\n",
+                             version, bytes);
+
+          response = malloc (length + 1);
+
+          snprintf (response, length + 1,
+                    "%s 200 OK\r\n"
+                    "Content-Type: text/html; charset=UTF-8\r\n"
+                    "Content-Length: %ld\r\n\r\n",
+                    version, bytes);
+        }
+      else if (!strcmp (version, "HTTP/1.1"))
+        {
+          length = snprintf (NULL, 0,
+                             "%s 200 OK\r\n"
+                             "Content-Type: text/html; charset=UTF-8\r\n"
+                             "Content-Length: %ld\r\n"
+                             "Connection: close\r\n\r\n",
+                             version, bytes);
+
+          response = malloc (length + 1);
+
+          snprintf (response, length + 1,
+                    "%s 200 OK\r\n"
+                    "Content-Type: text/html; charset=UTF-8\r\n"
+                    "Content-Length: %ld\r\n"
+                    "Connection: close\r\n\r\n",
+                    version, bytes);
+        }
+      char contents[BUFFER_LENGTH];
+      strcpy (contents, buffer);
+
+      response = realloc (response, length + bytes + 1);
+      strncat (response, contents, length + bytes + 1);
+    }
+  else
+    {
+      response = strdup ("HTTP/1.0 404 Not Found" CRLF CRLF);
+    }
+  return response;
+
+  // TODO [FULL]: Set the environment variables needed for the CGI programs
+  // located in cgi-bin. To do this, you will need to use either execve()
+  // or execle() when running the CGI program, using an array of string
+  // pairs. For example, the following array would set the db and record
+  // environment variables:
+  //
+  //   char *env[] = { "db=foo.txt", "record=2", NULL };
+  //
+  // If the request is a GET request, you should only set the QUERY_STRING
+  // variable to be the query parameter. For POST requests, you will need
+  // to look through the body of the HTTP request, splitting based on the
+  // boundary values (see the project description for an example).
+}
